677367f031bd4ebef1f68ac092391f66c1dcf0eb
allokrv <allokrv@gmail.com>
2020-06-19 08:37:37 +0200

    how to get client_secret prints

--- credManager.py
+++ credManager.py
@@ -104,7 +104,10 @@ class AppCredentials:
         else:
             print("Couldn't find a 'client_secret.json' please download, rename and put into ihuv2 folder\nYou can " +
                   "download / create a client_secret.json here: https://console.developers.google.com/apis/credentials")
-            print("At the top you should see a 'Create Credentials' button, create a Client-ID")
+            print("At the top you should see a 'Create Credentials' button, create a Client-ID\n"
+                  "It's gonna have a long, weird name so just rename it to 'client_secret.json'\n"
+                  "and put it into the main ihuv folder.")
+
             print("The Secret file is necessary for ihuv2 to work!")
             exit(1)
             return False
